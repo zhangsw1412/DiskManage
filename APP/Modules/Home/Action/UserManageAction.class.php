6c9a0de0a9c5688751890a1b549a22191210bb23
zhangsw <zhangsw1412.cpp@gmail.com>
2016-05-20 22:14:56 +0800

    用户状态信息

--- APP/Modules/Home/Action/UserManageAction.class.php
+++ APP/Modules/Home/Action/UserManageAction.class.php
@@ -6,4 +6,19 @@ class UserManageAction extends Action
 	{
 		
 	}
+
+	public function info()
+	{
+		if(is_null(session('uid')))
+			$this->redirect(GROUP_NAME.'/Login/index');
+		$result=M('user')->find(session('uid'));
+		if($result===false)
+			$this->error('数据库查询出错');
+		$this->username=$result['username'];
+		$this->login_time=$result['login_time'];
+		$this->login_ip=$result['login_ip'];
+		$this->valid=$result['valid'];
+		layout('Layout/standard');
+		$this->display();
+	}
 }
