2acfcbdaddedeb1be97c7cd369b5f0e0b2aea872
zhangsw <zhangsw1412.cpp@gmail.com>
2016-05-22 22:40:14 +0800

    盘片详细信息页完成

--- APP/Modules/Home/Action/DiskControlAction.class.php
+++ APP/Modules/Home/Action/DiskControlAction.class.php
@@ -25,6 +25,47 @@
             if($cds===false) $this->error('数据库查询失败');
             $this->cds=$cds;
             $this->page=$page->show();
+
+            layout('Layout/standard');
+            $this->display();
+        }
+
+        /**
+         * 盘片详情信息
+         * @return [type] [description]
+         */
+        public function diskDetail()
+        {
+            $rackId=(int)I('get.rackId');
+            $layerNum=(int)I('get.layerNum');
+            $columnNum=(int)I('get.columnNum');
+            $rowNum=(int)I('get.rowNum');
+            $cdNum=(int)I('get.cdNum');
+
+            $where=array(
+                    'tb_blockmodel.rackId'=>$rackId,
+                    'tb_blockmodel.layerNum'=>$layerNum,
+                    'tb_blockmodel.columnNum'=>$columnNum,
+                    'tb_blockmodel.rowNum'=>$rowNum,
+                    'tb_blockmodel.cdNum'=>$cdNum
+                );
+
+            import('Class.Page',APP_PATH);
+            $count=M('blockmodel')->join('LEFT JOIN tb_filemodel ON tb_blockmodel.fileId=tb_filemodel.fileId')->where($where)->count();
+            $page=new Page($count,15);
+            $limit=$page->firstRow.','.$page->listRows;
+            
+            $blocks=M('blockmodel')->join('tb_filemodel ON tb_blockmodel.fileId=tb_filemodel.fileId')->where($where)->field('tb_blockmodel.fileId,blockOffset,fileName')->limit($limit)->select();
+            if($blocks===false) $this->error('数据库查询失败');
+            $this->blocks=$blocks;
+            $this->page=$page->show();
+            $this->rackId=$rackId;
+            $this->layerNum=$layerNum;
+            $this->columnNum=$columnNum;
+            $this->rowNum=$rowNum;
+            $this->cdNum=$cdNum;
+            $this->step=$page->firstRow/$page->listRows+1;
+
             layout('Layout/standard');
             $this->display();
         }
