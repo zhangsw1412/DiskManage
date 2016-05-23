8ee6a36f4627bee9c3056c9f3e635d978ed271ad
zhangshanwen <zhangshanwen@mioji.com>
2016-05-23 19:17:30 +0800

    文件列表的查看和创建文件夹功能完成

--- APP/Modules/Home/Action/FileManageAction.class.php
+++ APP/Modules/Home/Action/FileManageAction.class.php
@@ -14,9 +14,89 @@ class FileManageAction extends Action
 	 * 浏览目录视图
 	 * @return [type] [description]
 	 */
-	public function browseDir()
+	public function browseDir($path='')
 	{
+		$path=empty($path)?C('ROOT_DIR'):$path;
+		$filesInDir=scandir($path);
+		$files=array();
+		foreach($filesInDir as $file)
+		{
+			if($file=='.'||$file=='..') continue;
+			$temp=array();
+			$stat=stat($path.'/'.$file);
+			if($file{0}==='.')
+			{
+				$temp['name']=substr_replace($file, "+", 0,1);
+			}
+			else
+			{
+				$temp['name']=$file;
+			}
+			$temp['isDir']=is_dir($path.'/'.$file)?1:0;
+			$temp['size']=$stat['size'];
+			$temp['atime']=$stat['atime'];
+			$temp['mtime']=$stat['mtime'];
+			$temp['ctime']=$stat['ctime'];
+
+			$files[]=$temp;
+		}
+		clearstatcache();
+
+		$this->path=str_replace('/', '-', $path);
+		$this->files=$files;
 		layout('Layout/standard');
-		$this->display();
+		$this->display('browseDir');
+	}
+
+	/**
+	 * 进入子目录
+	 * @return [type] [description]
+	 */
+	public function enterDir()
+	{
+		$base=I('get.base');
+		$sub=I('get.sub');
+		$base=str_replace('-', '/', $base);
+		if($sub{0}==='+')
+			$sub=substr_replace($sub, ".", 0, 1);
+		$this->browseDir($base.'/'.$sub);
+	}
+
+	/**
+	 * 回到上级目录
+	 * @return [type] [description]
+	 */
+	public function goBack()
+	{
+		$path=I('get.path');
+		$path=explode('-', $path);
+		array_pop($path);
+		$path=join('/',$path);
+		$path=strlen($path)<=strlen(C('ROOT_DIR'))?C('ROOT_DIR'):$path;
+		$this->browseDir($path);
+	}
+
+	public function createDir()
+	{
+		if(IS_POST)
+		{
+			$path=I('post.path');
+			$path=explode('-', $path);
+			$path=join('/',$path);
+			$folderName=I('post.folderName');
+			if(empty($folderName)||strpos($folderName, "/"))
+				$this->error('文件夹名不合法');
+			$pathFolder=$path.'/'.$folderName;
+			if(is_dir($pathFolder))
+				$this->error('文件夹已经存在');
+			if(mkdir($pathFolder,0755,true))
+				$this->success('创建成功');
+			else
+				$this->error('创建失败，请确认权限');
+		}
+		else
+		{
+			$this->error('出错啦');
+		}
 	}
 }
