3c1590865827af138b7f13919af7e2cbeeb3be7f
zhangshanwen <zhangshanwen@mioji.com>
2016-05-23 11:39:55 +0800

    更新文件管理菜单列表

--- APP/Modules/Home/Action/FileManageAction.class.php
+++ APP/Modules/Home/Action/FileManageAction.class.php
@@ -1,9 +1,22 @@
 <?php
 
+/**
+ * 文件管理控制器
+ */
 class FileManageAction extends Action
 {
 	public function index()
 	{
 		
+	}
+
+	/**
+	 * 浏览目录视图
+	 * @return [type] [description]
+	 */
+	public function browseDir()
+	{
+		layout('Layout/standard');
+		$this->display();
 	}
 }
